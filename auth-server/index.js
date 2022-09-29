fc095d8541e99abeadd1ec9eac3273a646a84daf
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-09-28 22:47:53 -0700

    updated with files from individual servers

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 require('dotenv').config();
-const app = require('./src/server.js');
 const { db } = require('./src/auth/models');
+const server = require('./src/server.js');
 
-db.sync().then(() => {
-  app.start(process.env.PORT || 3001);
-});
+db.sync()
+  .then(() => {
+    server.start(process.env.PORT || 3001);
+  });
--- auth-server/src/auth/middleware/bearer.js
+++ auth-server/src/auth/middleware/bearer.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const { users } = require('../models')
+const { users } = require('../models');
 
 module.exports = async (req, res, next) => {
 
+  if (!req.headers.authorization) { _authError(); }
+
   try {
 
-    if (!req.headers.authorization) { _authError() }
 
     const token = req.headers.authorization.split(' ').pop();
-    const validUser = await users.authenticateToken(token);
-    req.user = validUser;
-    req.token = validUser.token;
-    next();
+    const validUser = await users.authenticateBearer(token);
+    if (validUser){
+      req.user = validUser;
+      req.token = validUser.token;
+      next();
+    }
 
   } catch (e) {
     _authError();
@@ -21,4 +24,4 @@ module.exports = async (req, res, next) => {
   function _authError() {
     next('Invalid Login');
   }
-}
+};
